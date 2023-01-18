eb89eaa7510b9721792f6b13152715d0b4ce30a6
Johan Carrasco <johanmcm@hotmail.com>
2023-01-17 21:41:05 -0500

    CRUD completo

--- JCBurger/Views/JCBurgerListPage.xaml.cs
+++ JCBurger/Views/JCBurgerListPage.xaml.cs
@@ -7,7 +7,9 @@ public partial class JCBurgerListPage : ContentPage
 	public JCBurgerListPage()
 	{
 		InitializeComponent();
+        List<JCBurgers> burger = App.BurgerRepo.GetAllBurgers();
         BindingContext = this;
+        burgerList.ItemsSource = burger;
     }
 
     public void OnItemAdded(object sender, EventArgs e)
@@ -19,5 +21,10 @@ public partial class JCBurgerListPage : ContentPage
     {
         List<JCBurgers> burger = App.BurgerRepo.GetAllBurgers();
         burgerList.ItemsSource = burger;
+    }
+
+    async void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(JCBurgerItemPage), true, new Dictionary<string, object> { ["Item"] = (JCBurgers)e.CurrentSelection.FirstOrDefault() });
     }
 }
